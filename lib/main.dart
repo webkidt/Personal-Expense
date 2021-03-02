3a1596f1dd61c8d554215ec10c79d0aeb1a6fafd
David Pam <niratpam@gmail.com>
2021-03-02 05:00:46 +0100

    Fetching User Input (onChanged)

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  String titleInput;
+  String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -59,19 +61,22 @@ class MyHomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
+                    decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
                   ),
                   TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount',
-                    ),
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    onChanged: (val) => amountInput = val,
                   ),
                   FlatButton(
                     child: Text('Add Transaction'),
                     textColor: Colors.purple,
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleInput);
+                      print(amountInput);
+                    },
                   ),
                 ],
               ),
