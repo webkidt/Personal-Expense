424e4ff34b119c6a3d04d6303843868b7aaccfbb
David Pam <niratpam@gmail.com>
2021-03-02 06:25:40 +0100

    Connecting Widget, Managing Data, Adding Transaction to List

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './widgets/transaction_list.dart';
-import './widgets/new_transaction.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -39,8 +38,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          NewTransaction(),
-          TransactionList(),
+          UserTransactions(),
         ],
       ),
     );
