c87788ebe7a57e0e95bda7e10e14c414dcd0dc63
David Pam <niratpam@gmail.com>
2021-03-13 06:50:20 +0100

    Using 'const' Widgets & Constructors

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -19,7 +19,7 @@ class TransactionList extends StatelessWidget {
                   'No transactions added yet!',
                   style: Theme.of(context).textTheme.headline6,
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
                 Container(
@@ -44,7 +44,7 @@ class TransactionList extends StatelessWidget {
                   leading: CircleAvatar(
                     radius: 30,
                     child: Padding(
-                      padding: EdgeInsets.all(6),
+                      padding: const EdgeInsets.all(6),
                       child: FittedBox(
                         child: Text('\$${transactions[index].amount}'),
                       ),
@@ -59,15 +59,15 @@ class TransactionList extends StatelessWidget {
                   ),
                   trailing: MediaQuery.of(context).size.width > 460
                       ? TextButton.icon(
-                          label: Text('Delete'),
-                          icon: Icon(Icons.delete),
+                          label: const Text('Delete'),
+                          icon: const Icon(Icons.delete),
                           style: TextButton.styleFrom(
                             primary: Theme.of(context).errorColor,
                           ),
                           onPressed: () => deleteTx(transactions[index].id),
                         )
                       : IconButton(
-                          icon: Icon(Icons.delete),
+                          icon: const Icon(Icons.delete),
                           color: Theme.of(context).errorColor,
                           onPressed: () => deleteTx(transactions[index].id),
                         ),
